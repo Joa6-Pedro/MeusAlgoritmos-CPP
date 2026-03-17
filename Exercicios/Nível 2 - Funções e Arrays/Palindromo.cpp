38b97d41c3bccd6e4e08378018e4b0ad79709f72
Joa6-Pedro <joa6pedro@gmail.com>
2026-03-17 04:54:09 -0300

    Add Exercicio 2.1 - Palindromo

---
+++ Exercicios/Nível 2 - Funções e Arrays/Palindromo.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cctype>
+
+std::string formataPalavra(std::string);
+bool verificaPalindromo(std::string);
+
+int main() {
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    std::string palavra;
+
+    std::cout << "Digite uma palavra: ";
+    std::cin >> palavra;
+
+    palavra = formataPalavra(palavra);
+    std::cout << palavra << "\n" << std::endl;
+
+    bool palindromo = verificaPalindromo(palavra);
+
+    std::cout << "\nA palavra " << palavra << (palindromo? " é um palíndromo.": " não é um palíndromo.") 
+    << "\n" << std::endl;
+
+    system("Pause");
+    return 0;
+}
+
+
+std::string formataPalavra(std::string palavra) {
+
+    for (int i = 0; i < palavra.length(); i++){
+        palavra[i] = tolower(palavra[i]);
+    }
+
+    return palavra;
+}
+
+
+bool verificaPalindromo (std::string palavra) {
+    
+    bool palindromo = true;
+    int j = palavra.length() - 1;
+
+    for (int i = 0; i < palavra.length() / 2; i++) {
+
+        if (palavra[j] != palavra[i]) {
+            palindromo = false;
+            break;
+        }
+
+        j--;
+    }
+
+    return palindromo;
+}
